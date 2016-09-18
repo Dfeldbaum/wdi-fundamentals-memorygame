04ae1d223adb911db7dfaff1deb18a7feac2968f
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro.local>
2016-09-18 12:26:02 -0500

    Added new game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour){
+    alert('Sorry, try again.');
+} else if (cardOne === cardTwo){
+    alert('You found a match!');
+}
