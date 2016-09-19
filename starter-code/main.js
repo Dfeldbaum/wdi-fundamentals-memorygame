387dd10a03caf864e5feec7edd1ea5224b46743d
David Feldbaum <Dfeldbaum@Davids-MacBook-Pro.local>
2016-09-18 22:15:06 -0500

    CreatedJS function

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
-    alert('Sorry, try again.');
-} else if (cardOne === cardTwo){
-    alert('You found a match!');
+/*
+// if (cardTwo === cardFour){
+//    alert('Sorry, try again.');
+// } else if (cardOne === cardTwo){
+//    alert('You found a match!');
+*/
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
+
+
